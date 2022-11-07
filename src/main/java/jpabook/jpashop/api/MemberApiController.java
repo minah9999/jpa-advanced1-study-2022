fb099d5d3252c42ca00538b7172fe4e4a2c33bbb
minah9999 <81949526+minah9999@users.noreply.github.com>
2022-11-07 16:20:49 +0900

    [feat] 멤버 수정 api 구현

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import lombok.RequiredArgsConstructor;
@@ -36,6 +37,17 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 수정 V2
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
@@ -47,5 +59,18 @@ public class MemberApiController {
 
         public CreateMemberResponse(Long id) {
             this.id = id;
-        } }
+        }
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
 }
